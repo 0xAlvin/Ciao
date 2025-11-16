896fc3af60c0b52c93be32a6ec151e21e10855eb
Alvin <alvinvaati@gmail.com>
2025-11-16 19:33:36 +0300

    gitignore update

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -10,20 +10,20 @@ object Versions {
     const val KOTLIN = "2.1.0"
     const val KSP = "2.1.0-1.0.29"
 
-    const val CORE_KTX = "1.17.0"
-    const val LIFECYCLE = "2.9.4"
-    const val ACTIVITY_COMPOSE = "1.11.0"
+    const val CORE_KTX = "1.15.0"
+    const val LIFECYCLE = "2.8.7"
+    const val ACTIVITY_COMPOSE = "1.9.3"
 
-    const val COMPOSE_BOM = "2024.09.00"
+    const val COMPOSE_BOM = "2024.11.00"
 
-    const val HILT = "2.52"
+    const val HILT = "2.57.2"
     const val HILT_NAVIGATION_COMPOSE = "1.2.0"
 
-    const val NAVIGATION = "2.8.0"
+    const val NAVIGATION = "2.8.4"
 
     const val COROUTINES = "1.9.0"
 
     const val JUNIT = "4.13.2"
-    const val JUNIT_EXT = "1.3.0"
-    const val ESPRESSO = "3.7.0"
+    const val JUNIT_EXT = "1.2.1"
+    const val ESPRESSO = "3.6.1"
 }
