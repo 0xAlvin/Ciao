4c6a8e49eb99796f9e38633127b023fae08a9e2a
Alvin Vaati Mutuku <Alvin.Vaati@imbank.co.ke>
2025-11-19 12:55:32 +0300

    base class for usecases

--- core/domain/src/main/java/com.example.ciao.core.domain/usecase/base/UseCase.kt
+++ core/domain/src/main/java/com.example.ciao.core.domain/usecase/base/UseCase.kt
@@ -1,2 +1,17 @@
 package com.example.ciao.core.domain.usecase.base
 
+import kotlinx.coroutines.flow.Flow
+
+abstract class UseCase<in P, out R> {
+    protected abstract suspend fun execute(parameters: P): Result<R>
+    suspend operator fun invoke(params: P): Result<R> {
+        return execute(params)
+    }
+}
+
+abstract class FlowUseCase<in P, out R> {
+    protected abstract suspend fun execute(parameters: P): Flow<Result<R>>
+    suspend operator fun invoke(params: P): Flow<Result<R>> {
+        return execute(params)
+    }
+}
