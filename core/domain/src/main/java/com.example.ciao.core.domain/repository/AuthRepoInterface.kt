e62b2beeb34a8ecba355a4ad01f64e04e86b7e41
Alvin Vaati Mutuku <Alvin.Vaati@imbank.co.ke>
2025-11-19 15:33:33 +0300

    fix: Replaced common:result class in place of AuthResult

--- core/domain/src/main/java/com.example.ciao.core.domain/repository/AuthRepoInterface.kt
+++ core/domain/src/main/java/com.example.ciao.core.domain/repository/AuthRepoInterface.kt
@@ -1,14 +1,14 @@
 package com.example.ciao.core.domain.repository
 
-import AuthResult
+import com.example.ciao.common.result.Result
 import User
 import kotlinx.coroutines.flow.Flow
 
 interface AuthRepoInterface {
-    suspend fun signInWithEmail(email: String, password: String): AuthResult<User>
-    suspend fun signUpWithEmail(email: String, password: String): AuthResult<User>
-    suspend fun signOut(): AuthResult<Unit>
-    suspend fun resetPassword(email: String): AuthResult<Unit>
+    suspend fun signInWithEmail(email: String, password: String): Result<User>
+    suspend fun signUpWithEmail(email: String, password: String): Result<User>
+    suspend fun signOut(): Result<Unit>
+    suspend fun resetPassword(email: String): Result<Unit>
     fun getCurrentUser(): Flow<User?>
     fun isUserAuthenticated(): Boolean
 }
