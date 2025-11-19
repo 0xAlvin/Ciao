2984be78326f45acf40652e41f9cc76511bcc219
Alvin Vaati Mutuku <Alvin.Vaati@imbank.co.ke>
2025-11-19 15:35:59 +0300

    update: add login functionality to LoginUseCase for user

--- core/domain/src/main/java/com.example.ciao.core.domain/usecase/base/LoginUseCase.kt
+++ core/domain/src/main/java/com.example.ciao.core.domain/usecase/base/LoginUseCase.kt
@@ -1,16 +1,49 @@
 package com.example.ciao.core.domain.usecase.base
 
+import Email
+import Password
 import com.example.ciao.core.data.repository.AuthRepository
+import User
+import javax.inject.Inject
+import com.example.ciao.common.result.Result
+import isValid
 
 class LoginUseCase @Inject constructor(
     private  val authRepository: AuthRepository
-) : UseCase<LoginUseCase.Params, User> {
+) : UseCase<LoginUseCase.Params, User>() {
     override suspend fun execute(parameters: Params): Result<User> {
-        TODO("Not yet implemented")
+        if (parameters.email.value.isEmpty()) {
+            return Result.Error(
+                message = "Email can not be empty"
+            )
+        }
+        if (parameters.email.isValid(
+                email = parameters.email
+            )){
+            return Result.Error(
+                message = "Please enter a valid email address"
+            )
+        }
+        if (parameters.email.value.length < 6) {
+            return Result.Error(
+                message = "Password must be at-least 6 characters"
+            )
+        }
+        val response = authRepository.signInWithEmail(
+            email = parameters.email.value,
+            password = parameters.password.value
+        )
+        if (response.isError) {
+            return Result.Error(
+                message = "Login Failed"
+            )
+        }
+        return response
     }
 
     data class Params (
         val email: Email,
         val password: Password
     )
+
 }
