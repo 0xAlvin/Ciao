256f632e66f43d7b45fb82bf389316d95db9712c
Alvin Vaati Mutuku <Alvin.Vaati@imbank.co.ke>
2025-11-19 15:28:21 +0300

    fix:Usecase base class return Type

--- core/domain/src/main/java/com.example.ciao.core.domain/model/AuthResult.kt
+++
@@ -1,5 +0,0 @@
-sealed class AuthResult<out T>{
-    data class Success<T>(val data: T) : AuthResult<T>()
-    data class Error(val message: String) : AuthResult<Nothing>()
-    object Loading : AuthResult<Nothing>()
-}
--- core/domain/src/main/java/com.example.ciao.core.domain/usecase/base/UseCase.kt
+++ core/domain/src/main/java/com.example.ciao.core.domain/usecase/base/UseCase.kt
@@ -1,9 +1,10 @@
 package com.example.ciao.core.domain.usecase.base
 
 import kotlinx.coroutines.flow.Flow
+import com.example.ciao.common.result.Result
 
 abstract class UseCase<in P, out R> {
-    protected abstract suspend fun execute(parameters: P): Result<R>
+    protected abstract suspend fun execute(parameters: P): com.example.ciao.common.result.Result<R>
     suspend operator fun invoke(params: P): Result<R> {
         return execute(params)
     }
